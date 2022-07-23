7ac06e735b11b8ae1f62dfc1e39306bbffb5a2c8
Vy Nguyen <vy1872@gmail.com>
2022-07-22 22:36:52 -0700

    shows user if plan is past, present, or future now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
-$("#currentDay").text(moment().format("dddd, MMMM Do"))
+$("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+for (i = 9; i < 18; i++) {
+    if (moment().format("H") < 8) {
+        $("#" + i).addClass("future");
+    } else {
+        if (i < moment().format("H")){
+            $("#" + i).addClass("past");
+        } else if (i = moment().format("H")) {
+            $("#" + i).addClass("present");
+        } else {
+            $("#" + i).addClass("future");
+        }
+    }
+}
+
