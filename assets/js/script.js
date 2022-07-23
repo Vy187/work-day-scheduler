7998d7577e8363c07329afc104f0b67a22f9f53b
Vy Nguyen <vy1872@gmail.com>
2022-07-22 22:59:31 -0700

    puts data saved on each text box

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ for (i = 9; i < 18; i++) {
     if (moment().format("H") < 8) {
         $("#" + i).addClass("future");
     } else {
-        if (i < moment().format("H")){
+        if (i < moment().format("H")) {
             $("#" + i).addClass("past");
         } else if (i = moment().format("H")) {
             $("#" + i).addClass("present");
@@ -14,3 +14,12 @@ for (i = 9; i < 18; i++) {
     }
 }
 
+if (localStorage.getItem("plans") == null) {
+    plans = ["1", "2", "3", "4", "5", "6", "6", "7", "8"];
+    localStorage.setItem("plans", JSON.stringify(plans));
+} else {
+    data = JSON.parse(localStorage.getItem("plans"));
+    for (j = 0, k = 9; k < 18; j++, k++) {
+        $("#" + k).val(data[j]);
+    }
+}
